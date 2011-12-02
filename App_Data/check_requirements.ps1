f861c795bef97e022acb6490d32e48b27d157c1c
AlZaj <alzaj@yahoo.com>
2011-12-02 15:50:27 +0100

    add dir App_Data

---
+++ App_Data/check_requirements.ps1
@@ -0,0 +1,9 @@
+#to run this script you need PowerShell 2
+
+#- check installation of iis
+#- check the website for the directory with this script is configured
+#- check sql server is installed 
+#- prompt for database name and check if the database exists
+
+$strString = "Hello World"
+write-host $strString
